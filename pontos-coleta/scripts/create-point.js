716269f116c27e8dc32927e8e819ad28ea931b6d
Ralf Prezia <62730168+ralfprezia@users.noreply.github.com>
2020-10-16 19:53:31 -0300

    Add files via upload

---
+++ pontos-coleta/scripts/create-point.js
@@ -0,0 +1,63 @@
+
+
+function populateUFs() {
+    const ufSelect = document.querySelector("select[name=uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( res => res.json() )
+    .then(states => {
+
+        for( const state of states ) {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+    
+    } )
+}
+
+populateUFs()
+
+
+function getCities(event){
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+
+    const ufValue = event.target.value
+
+    //const indexOfSelectedState = event.target.selectedIndex
+    //stateInput.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/33/municipios`
+
+    citySelect.innerHTML = "<option value>Selecione a cidade</option>"
+    citySelect.disabled = true
+
+    fetch(url)
+    .then( res => res.json() )
+    .then( cities => {
+        
+        for( const city of cities ) {
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+        }
+
+        citySelect.disabled = false//função de zerar a caixa das cidades não está funcionando! PESQUISAR
+    } )
+}
+
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities)// ouvidor de eventos
+
+//ítens de coleta
+// pegar todos os <li>
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click",handleSelectedItem)
+}
+
+function handleSelectedItem(event) {
+    const itemId = event.target.dataset.id
+    console.log()
+}
+
